5f91a9217e5811d879c9aa1d25df9a2eb32770c8
biamsarmento <biamsarmento@gmail.com>
2025-12-10 15:26:11 -0300

    remove commented code

--- newsletter-quotes/app/layout.tsx
+++ newsletter-quotes/app/layout.tsx
@@ -2,24 +2,6 @@ import type { Metadata } from "next";
 import { gagalin, newYorker, garet, garetBold } from "./fonts";
 import "./globals.css";
 
-// const gagalin = localFont({
-//   src: "./fonts/Gagalin-Regular.otf",
-//   variable: "--font-gagalin",
-//   weight: "400",
-// });
-
-// const newYorker = localFont({
-//   src: "./fonts/ACaslonPro-Regular.otf",
-//   variable: "--font-new-yorker",
-//   weight: "400",
-// });
-
-// const garet = localFont({
-//   src: "./fonts/Spacetype-GaretBook.otf",
-//   variable: "--font-garet",
-//   weight: "400",
-// });
-
 export const metadata: Metadata = {
   title: "Quotes Newsletter",
   description: "Daily dose of inspiration.",
